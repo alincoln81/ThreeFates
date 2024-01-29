2ab8c2fb4cd618aad1e1b81da1a116de7299cbcd
alincoln81 <amberlove.lincoln@gmail.com>
2024-01-29 09:31:18 -0800

    Cleanup

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,118 +1,7 @@
-
-// Show/Hide Nav/Header on Scroll Up/Down
-/*
-var didScroll;
-var lastScrollTop = 0;
-var delta = 100;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    console.log(st);
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta) {
-        console.log('did not scroll more than delta');
-        return;
-    }
-    
-    if (st > lastScrollTop && st > navbarHeight){
-        console.log('hide nav');
-        // Scroll Down Hide Nav
-        $('header').removeClass('nav-down').addClass('nav-up');
-
-    } else {
-        // Scroll Up Show Nav
-        if(st + $(window).height() < $(document).height()) {
-            console.log('show nav');
-            $('header').removeClass('nav-up').addClass('nav-down');
-        }
-    }
-    
-    lastScrollTop = st;
-}
-*/
-//----------------------------------------------------------------------------------------------------------------------------
-
 // Check if touch device
-var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
-//console.log(isTouch);
+let isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
 
-// Animate About Section Buttons on Hover
-function startAnimText(el) {
-    //console.log('mousenter: ', el.target.id);
-
-    let targetSpans;
-
-    if(el.target.id === 'aboutDiv') {
-        targetSpans = sectionSpans[0];
-    } else if(el.target.id === 'servicesDiv') {
-        targetSpans = sectionSpans[1];
-    } else if(el.target.id === 'processDiv') {
-        targetSpans = sectionSpans[2];
-    } else if(el.target.id === 'portfolioDiv') {
-        targetSpans = sectionSpans[3];
-    } else {
-        console.log('Not Known');
-    }
-
-    //console.log(targetSpans);
-
-    for(var i = 0; i < targetSpans.length; i++) {
-        doSetTimeoutColor(targetSpans[i], i * 75, '#FFFFFF');
-        doSetTimeoutColor(targetSpans[i], (i+1) * 150, '#24a8c4');
-    }
-}
-function stopAnimText(el) {
-    //console.log('mouseleave: ', el.target.id);
-}
-// Change Text Color
-function doSetTimeoutColor(el, num, color) {
-    //console.log(el.innerHTML, num);
-    setTimeout(function() {
-        changeColor(el, color);
-    }, num);
-}
-function changeColor(e, color) {
-    //console.log(e);
-    e.style.color = color;
-}
-//-----------------------------------------------------------------------------------------------------------------------
-
-// Change Fill Color ----------------------------------------------------------
-function doSetTimeoutEyeFill(el, num, fill) {
-    setTimeout(function() {
-        changeFill(el, fill);
-    }, num);
-  }
-function changeFill(e, fill) {
-    e.style.fill = fill;
-}
-
-// Change Opacity -------------------------------------------------------------
-function doSetTimeoutEyeOpacity(el, num, opacity) {
-    setTimeout(function() {
-        changeOpacity(el, opacity);
-    }, num);
-}
-  function changeOpacity(e, opacity) {
-    e.style.opacity = opacity;
-}
-
-
-// Handle About Section Clicks -------------------------------------------------------------
+// Variables -------------------------------------------------------------
 let section = [
     document.getElementById('toggle-animation-about'),
     document.getElementById('toggle-animation-services'),
@@ -139,43 +28,56 @@ let sectionDivs = [
     document.getElementById('portfolioDiv')
 ];
 
-
-if(!isTouch) {
-    for(var m = 0; m < sectionDivs.length; m++) {
-        sectionDivs[m].addEventListener('mouseenter', startAnimText);
-        sectionDivs[m].addEventListener('mouseleave', stopAnimText);
-    }
-}
-
-
 let lastClicked = '';
 let fillColor = '#24a8c4';
 
-//SVG Manipulation
-let leftSideEye = document.getElementById('left-side');
-let leftSideEyePath = leftSideEye.getElementsByTagName('path');
-
-let rightSideEye = document.getElementById('right-side');
-let rightSideEyePath = rightSideEye.getElementsByTagName('path');
-
-
-let outerIris = document.getElementById('outer-iris');
-let outerIrisPaths = outerIris.getElementsByTagName('path');
-
-let midIris = document.getElementById('mid-iris-inner');
-let midIrisPaths = midIris.getElementsByTagName('path');
-
-let innerIris = document.getElementById('inner-iris');
-let innerIrisPaths = innerIris.getElementsByTagName('path');
+//SVG Variables ---------------------------------------
+let leftSideEye;
+let leftSideEyePath;
+let rightSideEye;
+let rightSideEyePath;
+//let outerIris;
+//let outerIrisPaths;
+let midIris;
+let midIrisPaths;
+let innerIris;
+let innerIrisPaths;
 
 let topSection = document.getElementById('about');
 
+// Initialize -------------------------------------------------------------------------------------------
+function init() {
+
+    if(!isTouch) {
+        for(var m = 0; m < sectionDivs.length; m++) {
+            sectionDivs[m].addEventListener('mouseenter', startAnimText);
+            sectionDivs[m].addEventListener('mouseleave', stopAnimText);
+        }
+    } 
+}
+
+// Functions ----------------------------------------------------------------------------------------------
 function clickedAbout(clicked) {
     //console.log('Clicked: ', clicked);
     //console.log('Last Clicked: ', lastClicked);
-    
 
     if(!isTouch) {
+
+        leftSideEye = document.getElementById('left-side');
+        leftSideEyePath = leftSideEye.getElementsByTagName('path');
+
+        rightSideEye = document.getElementById('right-side');
+        rightSideEyePath = rightSideEye.getElementsByTagName('path');
+
+        //outerIris = document.getElementById('outer-iris');
+        //outerIrisPaths = outerIris.getElementsByTagName('path');
+
+        midIris = document.getElementById('mid-iris-inner');
+        midIrisPaths = midIris.getElementsByTagName('path');
+
+        innerIris = document.getElementById('inner-iris');
+        innerIrisPaths = innerIris.getElementsByTagName('path');
+
         //Color Left & Right Eyes White or Blue based on Current Color
         if(fillColor === '#FFFFFF') {
             fillColor = '#24a8c4';
@@ -238,11 +140,7 @@ function clickedAbout(clicked) {
     } else {
         
         topSection.scrollIntoView({ behavior: "smooth" });
-        /*setTimeout(() => {
-            $('header').removeClass('nav-down').addClass('nav-up');
-        }, 500);
-        */
-
+  
         //Add Active Class to the last Clicked Button
         clickedBut.classList.add('active');
         //console.log('clicked new section, opening it.');
@@ -256,3 +154,67 @@ function clickedAbout(clicked) {
         lastClicked = clicked;
     }
 }
+
+// Animate About Section Buttons on Hover
+function startAnimText(el) {
+    //console.log('mousenter: ', el.target.id);
+
+    let targetSpans;
+
+    if(el.target.id === 'aboutDiv') {
+        targetSpans = sectionSpans[0];
+    } else if(el.target.id === 'servicesDiv') {
+        targetSpans = sectionSpans[1];
+    } else if(el.target.id === 'processDiv') {
+        targetSpans = sectionSpans[2];
+    } else if(el.target.id === 'portfolioDiv') {
+        targetSpans = sectionSpans[3];
+    } else {
+        console.log('Not Known');
+    }
+
+    //console.log(targetSpans);
+
+    for(var i = 0; i < targetSpans.length; i++) {
+        doSetTimeoutColor(targetSpans[i], i * 75, '#FFFFFF');
+        doSetTimeoutColor(targetSpans[i], (i+1) * 150, '#24a8c4');
+    }
+}
+function stopAnimText(el) {
+    //console.log('mouseleave: ', el.target.id);
+}
+
+// Change Text Color
+function doSetTimeoutColor(el, num, color) {
+    //console.log(el.innerHTML, num);
+    setTimeout(function() {
+        changeColor(el, color);
+    }, num);
+}
+function changeColor(e, color) {
+    //console.log(e);
+    e.style.color = color;
+}
+
+// Change Fill Color ----------------------------------------------------------
+function doSetTimeoutEyeFill(el, num, fill) {
+    setTimeout(function() {
+        changeFill(el, fill);
+    }, num);
+  }
+function changeFill(e, fill) {
+    e.style.fill = fill;
+}
+
+// Change Opacity -------------------------------------------------------------
+function doSetTimeoutEyeOpacity(el, num, opacity) {
+    setTimeout(function() {
+        changeOpacity(el, opacity);
+    }, num);
+}
+  function changeOpacity(e, opacity) {
+    e.style.opacity = opacity;
+}
+
+// Initialize -----------------------------------------------------------------------------------
+init();
